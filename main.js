f3eb2722e5a10ad5576dec06637e15e2d5a853d9
KajetanAmos <kajetan.kotlowski@gmail.com>
2020-10-25 14:52:26 +0100

    Dziala dodawanie komentarzy

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 window.addEventListener("load", ()=>{
     console.log("app has been loaded")
     const toggleButton = document.getElementById("toggle-article")
-    const addCommentButton = document.getElementById("add--comment--button")
+    const addCommentButton = document.getElementById("add-comment-button")
     toggleButton.addEventListener("click",()=>{
         const mainSection = document.querySelector(".article__main")
         const isArticleVisable = mainSection.style.display === "block" ? true : false
         toggleButton.innerHTML = `${!isArticleVisable ? "Hide" : "Show"} article`
         toggleMainSection(!isArticleVisable)
     })
-    addCommentButton.addEventListener("click"), () =>{
-
-    }
+    addCommentButton.addEventListener("click", () => {
+    addComment();
+    })
 });
 function toggleMainSection(show){
     const mainSection = document.querySelector(".article__main");
@@ -18,5 +18,16 @@ function toggleMainSection(show){
 }
 
 function addComment(){
+const nameInput = document.getElementById("name-input")
+const commentInput = document.getElementById("comment-input")
 
+const comment = {
+    name: nameInput.value,
+    content: commentInput.value
+}
+console.log(validateComment(comment))
+}
+function validateComment(comment){
+    const {name, content} = comment;
+    return name.length && content.length ? true :false
 }
