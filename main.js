8185aac2b39fa9443762cd28c995dbbf7f3ec6c4
KajetanAmos <kajetan.kotlowski@gmail.com>
2020-10-25 14:30:18 +0100

    tak

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 window.addEventListener("load", ()=>{
     console.log("app has been loaded")
     const toggleButton = document.getElementById("toggle-article")
-
+    const addCommentButton = document.getElementById("add--comment--button")
     toggleButton.addEventListener("click",()=>{
         const mainSection = document.querySelector(".article__main")
         const isArticleVisable = mainSection.style.display === "block" ? true : false
         toggleButton.innerHTML = `${!isArticleVisable ? "Hide" : "Show"} article`
         toggleMainSection(!isArticleVisable)
     })
+    addCommentButton.addEventListener("click"), () =>{
+
+    }
 });
 function toggleMainSection(show){
     const mainSection = document.querySelector(".article__main");
  mainSection.style.display = show ? "block" : "none"
 }
 
+function addComment(){
+
+}
